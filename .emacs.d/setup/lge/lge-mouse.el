4cdf2d18bce84ec6e91ebb0f666c74dd352f5f54
Li Ge <lge@ucsd.edu>
2013-11-18 22:44:47 -0800

    Update dotfiles

--- .emacs.d/setup/lge/lge-mouse.el
+++ .emacs.d/setup/lge/lge-mouse.el
@@ -1,14 +1,22 @@
 
 (global-set-key (kbd "<C-mouse-1>") 'ffap-at-mouse)
 
-(global-set-key (kbd "<C-down-mouse-5>")
-                (lambda ()
-                  (interactive)
-                  (text-scale-decrease 1)))
+(global-set-key (kbd "<C-down-mouse-5>") 'lge-text-scale-increase-1)
+(global-set-key (kbd "<C-down-mouse-4>") 'lge-text-scale-decrease-1)
 
-(global-set-key (kbd "<C-down-mouse-4>")
-                (lambda ()
-                  (interactive)
-                  (text-scale-increase 1)))
+;; mac
+(global-set-key (kbd "<C-wheel-up>") 'lge-text-scale-increase-1)
+(global-set-key (kbd "<C-wheel-down>") 'lge-text-scale-decrease-1)
+
+
+(defun lge-text-scale-decrease-1 ()
+  "DOCSTRING"
+  (interactive)
+  (text-scale-decrease 1))
+
+(defun lge-text-scale-increase-1 ()
+  "DOCSTRING"
+  (interactive)
+  (text-scale-increase 1))
 
 (provide 'lge-mouse)
