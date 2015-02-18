f8bfd45bd0a7f673f7c7eae43a0c021febdd638e
Li Ge <lge@ucsd.edu>
2015-02-18 12:23:59 -0800

    improve emmet configuration

--- emacs.d/lge/lge-emmet.el
+++ emacs.d/lge/lge-emmet.el
@@ -1,14 +1,20 @@
 (require-package 'emmet-mode)
 (require 'emmet-mode)
 
-(add-hook 'web-mode-hook 'emmet-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
-(add-hook 'sgml-mode-hook (lambda ()
-                            (yas-minor-mode 0)))
-(add-hook 'css-mode-hook (lambda ()
-                            (yas-minor-mode 0)))
-(add-hook 'web-mode-hook (lambda ()
-                            (yas-minor-mode 0)))
+(let ((hooks
+       (list
+        'web-mode-hook
+        'sgml-model-hook
+        'css-mode-hook
+        )))
+  (mapc
+   (lambda (m)
+     (add-hook m 'emmet-mode)
+     (add-hook m (lambda () yas-minor-mode 0))
+     ) hooks))
+
+;; Need to add to the emmet source code manually
+;; ~/.emacs.d/elpa/emmet-mode-201xxxxx.xxxx/emmet-mode.el:931
+;; (puthash "ff:h" "font-family:\"Helvetica Neue\", Helvetica, Arial, sans-serif;" tbl)
 
 (provide 'lge-emmet)
